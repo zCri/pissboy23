50fb4fb221891250a9264bf6ddae8a4cbf44227b
dskprt <59660013+dskprt@users.noreply.github.com>
2021-06-10 13:05:23 +0200

    ok zcri

--- CPU.cs
+++ CPU.cs
@@ -1,15 +1,64 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace pissboy23 {
-    class CPU : Component {
-        public byte A, B, C, D, E, H, L, F;
-        public ushort PC, SP;
 
-        public void reset() {
-        }
+    public class CPU : Component {
+
+        public static Clock clock;
+        public static Registers registers;
+
+        public Func<int>[] opcodes8 = new Func<int>[] { // returns machine cycles taken
+            () => { // 0x00 NOP
+                registers.pc++;
+                return 1;
+            },
+            () => { // 0x01 LD BC, d16
+                registers.b = GameBoy.memory.ReadByte(registers.pc);
+                registers.pc++;
+                registers.c = GameBoy.memory.ReadByte(registers.pc);
+                registers.pc++;
+                return 3;
+            },
+            () => { // 0x02 LD (BC), A
+                ushort address = (ushort) ((registers.c << 8) + registers.b);
+                GameBoy.memory.WriteByte(address, registers.a);
+                return 2;
+            },
+            () => { // 0x03 INC BC
+                registers.b++;
+                registers.c++;
+                return 2;
+            },
+            () => { // 0x04 INC B
+                registers.b++;
+                return 1;
+            },
+            () => { // 0x05 DEC B
+                registers.b--;
+                return 1;
+            },
+            () => { // 0x06 LD B, d8
+                registers.b = GameBoy.memory.ReadByte(registers.pc);
+                registers.pc++;
+                return 2;
+            },
+            () => { // 0x07 RLCA
+                registers.a = (byte) ((registers.a >> 7) | ((registers.a & 0x7f) << 1));
+                return 1;
+            }
+        };
+        
+        public void reset(){}
+    }
+
+    public struct Clock {
+        public int m, t;
+    }
+
+    public struct Registers {
+        public byte a, b, c, d, e, h, l, f;
+        public ushort pc, sp;
+        public int m, t;
     }
 }
